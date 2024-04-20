6d83f1a7f08a309de198de1f93cc190e34978e48
zahimkhan18 <159295861+zahimkhan18@users.noreply.github.com>
2024-04-20 23:40:33 +0500

    chap 17 to 18 assignment'

---
+++ app.js
@@ -0,0 +1,78 @@
+//  Task-1
+
+// let multiArray = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+// console.log( multiArray )
+
+// Task-2
+
+// var multiArray = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ];
+// console.log( multiArray )
+
+// Task-3
+
+// for(var i = 1 ; i<=10 ;i++){
+//     console.log(i)
+// }
+
+// Task-4
+
+// var tableNumber = +prompt("enter table number:");
+// var tablelen = +prompt("enter table length:");
+// console.log(tableNumber, "tableNumber");
+
+// for (var i = 1; i <= tablelen; i++) {
+//   document.write(tableNumber + "x" + i + "=" + tableNumber * i + "<br />");
+// }
+
+// Task-5
+
+// let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//     document.write(fruits[i] + "<br />");
+// }
+
+// Task-6
+
+// // a. Counting: 1 to 15
+// document.write("a. Counting: ");
+// for (let i = 1; i <= 15; i++) {
+//     document.write(i + ", ");
+// }
+// document.write("<br>");
+
+// // b. Reverse counting: 10 to 1
+// document.write("b. Reverse counting: ");
+// for (let i = 10; i >= 1; i--) {
+//     document.write(i + ", ");
+// }
+// document.write("<br>");
+
+// // c. Even numbers: 0 to 20
+// document.write("c. Even: ");
+// for (let i = 0; i <= 20; i += 2) {
+//     document.write(i + ", ");
+// }
+// document.write("<br>");
+
+// // d. Odd numbers: 1 to 19
+// document.write("d. Odd: ");
+// for (let i = 1; i <= 19; i += 2) {
+//     document.write(i + ", ");
+// }
+// document.write("<br>");
+
+// // e. Series: 2k to 20k
+// document.write("e. Series: ");
+// for (let i = 2; i <= 20; i += 2) {
+//     document.write(i + "k, ");
+// }
+// document.write("<br>");
